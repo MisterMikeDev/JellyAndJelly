43a4d299cd655864f263c6b33d2216c9b5e0e6de
MisterMikeDev <diegoerr04@gmail.com>
2024-05-15 00:09:22 -0600

    :sparkles: Add vercel adapter

--- .vercel/output/functions/_render.func/mnt/c/Dev/J&J/.vercel/output/_functions/chunks/_slug__Cw4Qlvob.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/_slug__BZ1wpYwi.mjs').then(n => n._);
-
-export { page };
--- .vercel/output/static/_astro/hoisted.B4NI-NX0.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.B8e_A8KY.js";const e=document.getElementById("nextSection"),t=document.querySelector("#home");e?.addEventListener("click",()=>{t?.scrollIntoView({behavior:"smooth"})});
